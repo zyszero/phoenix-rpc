fd1cedf3399bb7180602f1a949b477a95465cfbd
zyszero <346124796@qq.com>
2024-03-25 23:52:28 +0800

    add router interface desc.

--- phoenix-rpc-core/src/main/java/cn/zyszero/phoenix/rpc/core/api/Router.java
+++ phoenix-rpc-core/src/main/java/cn/zyszero/phoenix/rpc/core/api/Router.java
@@ -3,6 +3,12 @@ package cn.zyszero.phoenix.rpc.core.api;
 import java.util.List;
 
 /**
+ * router 的作用：
+ * 本质上来说从一堆中选出一小部分，即从一个集合中选出一个集合的能力，是从 LoadBalancer 中抽象出来的能力
+ * 比如：
+ * 有机房 A、机房 B、机房 C，机房 A 的实例都打上标签 a、以此类推，机房 B =》实例 b，机房 C =》实例 c；
+ * 假设 A 机房最近，我希望可以就近访问实例，那么选出所有标签 a 的实例的能力就是 Router 的作用，
+ * 下一步才是通过 LoadBalancer 选择一个实例进行访问
  * @Author: zyszero
  * @Date: 2024/3/24 19:00
  */
