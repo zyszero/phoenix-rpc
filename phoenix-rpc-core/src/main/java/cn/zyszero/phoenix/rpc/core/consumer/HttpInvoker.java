858805d4166a05cbd0a5aebb67b87a0cfb3c11f5
zyszero <346124796@qq.com>
2024-05-11 06:30:16 +0800

    add httpGet method.

--- phoenix-rpc-core/src/main/java/cn/zyszero/phoenix/rpc/core/consumer/HttpInvoker.java
+++ phoenix-rpc-core/src/main/java/cn/zyszero/phoenix/rpc/core/consumer/HttpInvoker.java
@@ -4,6 +4,7 @@ import cn.zyszero.phoenix.rpc.core.api.RpcRequest;
 import cn.zyszero.phoenix.rpc.core.api.RpcResponse;
 import cn.zyszero.phoenix.rpc.core.consumer.http.OkHttpInvoker;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,7 +36,15 @@ public interface HttpInvoker {
 
 
     @SneakyThrows
-    static <T> T httpPost(String requestString,String url, Class<T> clazz) {
+    static <T> T httpGet(String url, TypeReference<T> typeReference) {
+        log.debug(" =====>>>>>> httpGet: " + url);
+        String respJson = Default.get(url);
+        log.debug(" =====>>>>>> response: " + respJson);
+        return JSON.parseObject(respJson, typeReference);
+    }
+
+    @SneakyThrows
+    static <T> T httpPost(String requestString, String url, Class<T> clazz) {
         log.debug(" =====>>>>>> httpGet: " + url);
         String respJson = Default.post(requestString, url);
         log.debug(" =====>>>>>> response: " + respJson);
